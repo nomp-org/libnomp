bf201bbdf874b9d0c3f2f5d5f21d9a9e00c483f6
Thilina Ratnayaka <thilinarmtb@gmail.com>
2022-10-09 12:30:16 -0500

    Refactor nomp-api-100 to get rid of duplicate code

---
+++ tests/nomp-api-100.c
@@ -0,0 +1,57 @@
+#include "nomp.h"
+#include <math.h>
+#include <stdlib.h>
+
+#define TEST_TYPE int
+#define TEST_SUFFIX _int
+#include "nomp-api-100-impl.h"
+#undef TEST_TYPE
+#undef TEST_SUFFIX
+
+#define TEST_TYPE long
+#define TEST_SUFFIX _long
+#include "nomp-api-100-impl.h"
+#undef TEST_TYPE
+#undef TEST_SUFFIX
+
+#define TEST_TYPE unsigned
+#define TEST_SUFFIX _unsigned
+#include "nomp-api-100-impl.h"
+#undef TEST_TYPE
+#undef TEST_SUFFIX
+
+#define TEST_TOL 1e-12
+#define TEST_TYPE double
+#define TEST_SUFFIX _double
+#include "nomp-api-100-impl.h"
+#undef TEST_TYPE
+#undef TEST_SUFFIX
+#undef TEST_TOL
+
+#define TEST_TOL 1e-8
+#define TEST_TYPE float
+#define TEST_SUFFIX _float
+#include "nomp-api-100-impl.h"
+#undef TEST_TYPE
+#undef TEST_SUFFIX
+#undef TEST_TOL
+
+int main(int argc, char *argv[]) {
+  char *backend = argc > 1 ? argv[1] : "opencl";
+  int device = argc > 2 ? atoi(argv[2]) : 0;
+  int platform = argc > 3 ? atoi(argv[3]) : 0;
+
+  int err = nomp_init(backend, device, platform);
+  nomp_chk(err);
+
+  nomp_api_100_int();
+  nomp_api_100_long();
+  nomp_api_100_unsigned();
+  nomp_api_100_double();
+  nomp_api_100_float();
+
+  err = nomp_finalize();
+  nomp_chk(err);
+
+  return 0;
+}
