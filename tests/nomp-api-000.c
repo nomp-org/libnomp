ba97ea8dbb23ee8d9f37abaedacc250c52fb4ab4
nipunayf <nipuna.18@cse.mrt.ac.lk>
2022-12-03 02:11:43 +0530

    Print status of subtests

--- tests/nomp-api-000.c
+++ tests/nomp-api-000.c
@@ -1,46 +1,71 @@
 #include "nomp-test.h"
-#include "nomp.h"
+
+// Calling `nomp_finalize` before `nomp_init` should return an error
+static int test_first_nomp_finalize() {
+  int output = 0;
+
+  int err = nomp_finalize();
+  nomp_test_assert(&output,
+                   nomp_get_log_no(err) == NOMP_RUNTIME_NOT_INITIALIZED);
+
+  char *desc;
+  nomp_get_log_str(&desc, err);
+  int matched = match_log(desc, "\\[Error\\] .*libnomp\\/src\\/nomp.c:[0-9]* "
+                                "libnomp is not initialized.");
+  nomp_test_assert(&output, matched);
+  tfree(desc);
+  return output;
+}
+
+// Calling `nomp_init` twice must return an error, but must not segfault
+static int test_nomp_init_twice(char *backend, int device, int platform) {
+  int output = 0;
+
+  int err = nomp_init(backend, platform, device);
+  nomp_test_chk(&output, err);
+  err = nomp_init(backend, platform, device);
+  nomp_test_assert(&output,
+                   nomp_get_log_no(err) == NOMP_RUNTIME_ALREADY_INITIALIZED);
+
+  char *desc;
+  nomp_get_log_str(&desc, err);
+  int matched =
+      match_log(desc, "\\[Error\\] .*libnomp\\/src\\/nomp.c:[0-9]* libnomp is "
+                      "already initialized to use opencl. Call nomp_finalize() "
+                      "before calling nomp_init() again.");
+  nomp_test_assert(&output, matched);
+  tfree(desc);
+  return output;
+}
+
+// Calling `nomp_finalize` twice must return an error, but must not segfault
+static int test_nomp_finalize_twice() {
+  int output = 0;
+
+  int err = nomp_finalize();
+  nomp_test_chk(&output, err);
+  err = nomp_finalize();
+  nomp_test_assert(&output,
+                   nomp_get_log_no(err) == NOMP_RUNTIME_NOT_INITIALIZED);
+
+  char *desc;
+  nomp_get_log_str(&desc, err);
+  int matched = match_log(desc, "\\[Error\\] .*libnomp\\/src\\/nomp.c:[0-9]* "
+                                "libnomp is not initialized.");
+  nomp_test_assert(&output, matched);
+  tfree(desc);
+  return output;
+}
 
 int main(int argc, char *argv[]) {
   char *backend;
   int device, platform;
   parse_input(argc, argv, &backend, &device, &platform);
+  int output = 0;
 
-  // Calling `nomp_finalize` before `nomp_init` should return an error
-  int err = nomp_finalize();
-  nomp_assert(nomp_get_log_no(err) == NOMP_RUNTIME_NOT_INITIALIZED);
+  PRINT_SUBTEST(output, test_first_nomp_finalize)
+  PRINT_SUBTEST(output, test_nomp_init_twice, backend, device, platform);
+  PRINT_SUBTEST(output, test_nomp_finalize_twice);
 
-  char *desc;
-  err = nomp_get_log_str(&desc, err);
-  int matched = match_log(desc, "\\[Error\\] .*libnomp\\/src\\/nomp.c:[0-9]* "
-                                "libnomp is not initialized.");
-  nomp_assert(matched);
-
-  // Calling `nomp_init` twice must return an error, but must not segfault
-  err = nomp_init(backend, platform, device);
-  nomp_chk(err);
-  err = nomp_init(backend, platform, device);
-  nomp_assert(nomp_get_log_no(err) == NOMP_RUNTIME_ALREADY_INITIALIZED);
-
-  err = nomp_get_log_str(&desc, err);
-  matched =
-      match_log(desc, "\\[Error\\] .*libnomp\\/src\\/nomp.c:[0-9]* libnomp is "
-                      "already initialized to use opencl. Call nomp_finalize() "
-                      "before calling nomp_init() again.");
-  nomp_assert(matched);
-
-  // Calling `nomp_finalize` twice must return an error, but must not segfault
-  err = nomp_finalize();
-  nomp_chk(err);
-  err = nomp_finalize();
-  nomp_assert(nomp_get_log_no(err) == NOMP_RUNTIME_NOT_INITIALIZED);
-
-  err = nomp_get_log_str(&desc, err);
-  matched = match_log(desc, "\\[Error\\] .*libnomp\\/src\\/nomp.c:[0-9]* "
-                            "libnomp is not initialized.");
-  nomp_assert(matched);
-
-  tfree(desc);
-
-  return 0;
+  return output;
 }
