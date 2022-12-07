6f59405b1489efe7c935f8b9cf020ebd6dc34358
nipunayf <nipuna.18@cse.mrt.ac.lk>
2022-12-07 23:08:00 +0530

    Remove the error details from the console

--- tests/nomp-test.h
+++ tests/nomp-test.h
@@ -17,55 +17,30 @@
 
 #define TEST_BUILTIN_TYPES(a, ...)                                             \
   {                                                                            \
-    TOKEN_PASTE(TOKEN_PASTE(nomp_api_, a), _int(__VA_ARGS__);)                 \
-    TOKEN_PASTE(TOKEN_PASTE(nomp_api_, a), _long(__VA_ARGS__);)                \
-    TOKEN_PASTE(TOKEN_PASTE(nomp_api_, a), _unsigned(__VA_ARGS__);)            \
-    TOKEN_PASTE(TOKEN_PASTE(nomp_api_, a), _unsigned_long(__VA_ARGS__);)       \
-    TOKEN_PASTE(TOKEN_PASTE(nomp_api_, a), _double(__VA_ARGS__);)              \
-    TOKEN_PASTE(TOKEN_PASTE(nomp_api_, a), _float(__VA_ARGS__);)               \
+    TOKEN_PASTE(TOKEN_PASTE(nomp_api_, a), _int)(__VA_ARGS__);                 \
+    TOKEN_PASTE(TOKEN_PASTE(nomp_api_, a), _long)(__VA_ARGS__);                \
+    TOKEN_PASTE(TOKEN_PASTE(nomp_api_, a), _unsigned)(__VA_ARGS__);            \
+    TOKEN_PASTE(TOKEN_PASTE(nomp_api_, a), _unsigned_long)(__VA_ARGS__);       \
+    TOKEN_PASTE(TOKEN_PASTE(nomp_api_, a), _double)(__VA_ARGS__);              \
+    TOKEN_PASTE(TOKEN_PASTE(nomp_api_, a), _float)(__VA_ARGS__);               \
   }
 
-#define PRINT_SUBTEST(output, subtest, ...)                                    \
-  {                                                                            \
-    int err = (subtest)(__VA_ARGS__);                                          \
-    char *result = err ? "\033[31mFailed" : "\033[32mPassed";                  \
-    printf("\t%s: %s\033[0m\n", TOSTRING(subtest), result);                    \
-    print_error(err);                                                          \
-    output |= err;                                                             \
-  }
+#define SUBTEST(subtest, ...) subtest_(subtest(__VA_ARGS__), TOSTRING(subtest))
+static int subtest_(int err, const char *test_name) {
+  char *result = err ? "\033[31mFailed" : "\033[32mPassed";
+  printf("\t%s: %s\033[0m\n", test_name, result);
+  return err;
+}
 
-#define MAX_NOMP_ERROR_DETAILS 200
-static char *error_detail;
-
-#define nomp_test_assert(output, cond)                                         \
-  nomp_test_assert_(output, cond, __FILE__, __LINE__)
-static void nomp_test_assert_(int *output, int cond, const char *file,
-                              unsigned line) {
+static void nomp_test_assert(int *output, int cond) {
   if (!cond && *output == 0) {
-    error_detail = tcalloc(char, MAX_NOMP_ERROR_DETAILS);
-    snprintf(error_detail, MAX_NOMP_ERROR_DETAILS,
-             "\tnomp_assert failure at %s:%d\n\n", file, line);
     *output = 1;
   }
 }
 
-#define nomp_test_chk(output, err)                                             \
-  nomp_test_chk_(output, err, __FILE__, __LINE__)
-static void nomp_test_chk_(int *output, int err_id, const char *file,
-                           unsigned line) {
+static void nomp_test_chk(int *output, int err_id) {
   if (nomp_get_log_type(err_id) == NOMP_ERROR && *output == 0) {
-    char *err_str;
-    nomp_get_log_str(&err_str, err_id);
-    printf("\t%s:%d %s\n\n", file, line, err_str);
-    tfree(err_str);
     *output = 1;
-  }
-}
-
-static void print_error(int output) {
-  if (output == 1) {
-    printf("%s", error_detail);
-    tfree(error_detail);
   }
 }
 
