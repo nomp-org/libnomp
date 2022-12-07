80dedf360414e298f2f0c553d5b12a35891333df
nipunayf <nipuna.18@cse.mrt.ac.lk>
2022-12-08 00:14:39 +0530

    Remove the output variable from test assertions

--- tests/nomp-test.h
+++ tests/nomp-test.h
@@ -15,34 +15,34 @@
 #define TOSTRING_(x) #x
 #define TOSTRING(x) TOSTRING_(x)
 
-#define TEST_BUILTIN_TYPES(a, ...)                                             \
+#define TEST_BUILTIN_TYPES(api, ...)                                           \
   {                                                                            \
-    TOKEN_PASTE(TOKEN_PASTE(nomp_api_, a), _int)(__VA_ARGS__);                 \
-    TOKEN_PASTE(TOKEN_PASTE(nomp_api_, a), _long)(__VA_ARGS__);                \
-    TOKEN_PASTE(TOKEN_PASTE(nomp_api_, a), _unsigned)(__VA_ARGS__);            \
-    TOKEN_PASTE(TOKEN_PASTE(nomp_api_, a), _unsigned_long)(__VA_ARGS__);       \
-    TOKEN_PASTE(TOKEN_PASTE(nomp_api_, a), _double)(__VA_ARGS__);              \
-    TOKEN_PASTE(TOKEN_PASTE(nomp_api_, a), _float)(__VA_ARGS__);               \
+    TOKEN_PASTE(TOKEN_PASTE(nomp_api_, api), _int)(__VA_ARGS__);               \
+    TOKEN_PASTE(TOKEN_PASTE(nomp_api_, api), _long)(__VA_ARGS__);              \
+    TOKEN_PASTE(TOKEN_PASTE(nomp_api_, api), _unsigned)(__VA_ARGS__);          \
+    TOKEN_PASTE(TOKEN_PASTE(nomp_api_, api), _unsigned_long)(__VA_ARGS__);     \
+    TOKEN_PASTE(TOKEN_PASTE(nomp_api_, api), _double)(__VA_ARGS__);            \
+    TOKEN_PASTE(TOKEN_PASTE(nomp_api_, api), _float)(__VA_ARGS__);             \
   }
 
-#define SUBTEST(subtest, ...) subtest_(subtest(__VA_ARGS__), TOSTRING(subtest))
 static int subtest_(int err, const char *test_name) {
   char *result = err ? "\033[31mFailed" : "\033[32mPassed";
   printf("\t%s: %s\033[0m\n", test_name, result);
   return err;
 }
+#define SUBTEST(subtest, ...) subtest_(subtest(__VA_ARGS__), TOSTRING(subtest))
 
-static void nomp_test_assert(int *output, int cond) {
-  if (!cond && *output == 0) {
-    *output = 1;
+#define nomp_test_assert(cond)                                                 \
+  {                                                                            \
+    if (!(cond))                                                               \
+      return 1;                                                                \
   }
-}
 
-static void nomp_test_chk(int *output, int err_id) {
-  if (nomp_get_log_type(err_id) == NOMP_ERROR && *output == 0) {
-    *output = 1;
+#define nomp_test_chk(err_id)                                                  \
+  {                                                                            \
+    if (nomp_get_log_type((err_id)) == NOMP_ERROR)                             \
+      return 1;                                                                \
   }
-}
 
 static int match_log(const char *log, const char *pattern) {
   regex_t regex;
