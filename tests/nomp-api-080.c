6f8657a50bb3728731ee2f626b9a4478b8ef9bec
pubuduprabhathiya <pubuduprabhathiya@gmail.com>
2022-12-08 13:52:14 +0530

    Add tests for ignore invaild arguments

--- tests/nomp-api-080.c
+++ tests/nomp-api-080.c
@@ -4,18 +4,33 @@
 
 int main(int argc, const char *argv[]) {
 
+  // Ignore invaild arguments
+  const char *args1[] = {"foo.c", "-b", "opencl", "-d", "0", "-p", "0"};
+  argc = 7;
+  int err = nomp_init(argc, args1);
+  nomp_chk(err);
+  err = nomp_finalize();
+  nomp_chk(err);
+
+  const char *args2[] = {"-b", "opencl", "cuda", "-d", "0", "1", "-p", "0"};
+  argc = 8;
+  err = nomp_init(argc, args2);
+  nomp_chk(err);
+  err = nomp_finalize();
+  nomp_chk(err);
+
   // Invaild argument should return an error
-  const char *args[] = {"--b", "opencl", "-d", "0", "-p", "0"};
+  const char *args3[] = {"--b", "opencl", "-d", "0", "-p", "0"};
   argc = 6;
-  int err = nomp_init(argc, args);
+  err = nomp_init(argc, args3);
   nomp_assert(nomp_get_log_no(err) == NOMP_USER_ARGS_IS_INVALID);
   err = nomp_finalize();
   nomp_assert(nomp_get_log_no(err) == NOMP_RUNTIME_NOT_INITIALIZED);
 
   // Missing value should return an error
-  const char *args3[] = {"-b", "opencl", "-d", "0", "-p"};
+  const char *args4[] = {"-b", "opencl", "-d", "0", "-p"};
   argc = 5;
-  err = nomp_init(argc, args3);
+  err = nomp_init(argc, args4);
   nomp_assert(nomp_get_log_no(err) == NOMP_USER_ARGS_IS_INVALID);
   err = nomp_finalize();
   nomp_assert(nomp_get_log_no(err) == NOMP_RUNTIME_NOT_INITIALIZED);
