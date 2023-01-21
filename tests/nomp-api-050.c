716f05fb9511071c7b0ff926f7ec527e30e64d47
Thilina Ratnayaka <thilinarmtb@gmail.com>
2023-01-21 15:40:02 -0600

    Add builtin types to test-050
    and minor refactor.

--- tests/nomp-api-050.c
+++ tests/nomp-api-050.c
@@ -1,113 +1,71 @@
 #include "nomp-test.h"
 #include "nomp.h"
 
+#define TEST_IMPL_H "nomp-api-050-impl.h"
+#include "nomp-generate-tests.h"
+#undef TEST_IMPL_H
+
 // Free'ing before mapping should return an error.
 static int test_free_before_mapping() {
-  int a[10] = {0};
-  int err = nomp_update(a, 0, 10, sizeof(int), NOMP_FREE);
-  nomp_test_assert(nomp_get_log_no(err) == NOMP_USER_MAP_OP_IS_INVALID);
-
-  char *desc;
-  nomp_get_log_str(&desc, err);
-  int matched = match_log(
-      desc, "\\[Error\\] "
-            ".*libnomp\\/src\\/nomp.c:[0-9]* NOMP_FREE or NOMP_FROM can only "
-            "be called on a pointer which is already on the device.");
-  tfree(desc);
-  nomp_test_assert(matched);
-
-  return 0;
+  int err = 0;
+  TEST_BUILTIN_TYPES(050_free_before_mapping, 0, 10);
+  TEST_BUILTIN_TYPES(050_free_before_mapping, 5, 10);
+  TEST_BUILTIN_TYPES(050_free_before_mapping, 2, 8);
+  return err;
 }
 
 // D2H before H2D should return an error.
 static int test_d2h_before_h2d() {
-  int a[10] = {0};
-  int err = nomp_update(a, 0, 10, sizeof(int), NOMP_FROM);
-  nomp_test_assert(nomp_get_log_no(err) == NOMP_USER_MAP_OP_IS_INVALID);
-
-  char *desc;
-  nomp_get_log_str(&desc, err);
-  int matched = match_log(
-      desc, "\\[Error\\] "
-            ".*libnomp\\/src\\/nomp.c:[0-9]* NOMP_FREE or NOMP_FROM can only "
-            "be called on a pointer which is already on the device.");
-  tfree(desc);
-  nomp_test_assert(matched);
-
-  return 0;
+  int err = 0;
+  TEST_BUILTIN_TYPES(050_d2h_before_h2d, 0, 10);
+  TEST_BUILTIN_TYPES(050_d2h_before_h2d, 5, 10);
+  TEST_BUILTIN_TYPES(050_d2h_before_h2d, 2, 8);
+  return err;
 }
 
 // Mapping H2D multiple times is not an error.
 static int test_multiple_h2d_calls() {
-  int a[10] = {0};
-  int err = nomp_update(a, 0, 10, sizeof(int), NOMP_TO);
-  nomp_test_chk(err);
-  err = nomp_update(a, 0, 10, sizeof(int), NOMP_TO);
-  nomp_test_chk(err);
-
-  return 0;
+  int err = 0;
+  TEST_BUILTIN_TYPES(050_multiple_h2d_calls, 0, 10);
+  TEST_BUILTIN_TYPES(050_multiple_h2d_calls, 5, 10);
+  TEST_BUILTIN_TYPES(050_multiple_h2d_calls, 2, 8);
+  return err;
 }
 
 // Mapping D2H multiple times is not an error.
 static int test_multiple_d2h_calls() {
-  int a[10] = {0};
-  int err = nomp_update(a, 0, 10, sizeof(int), NOMP_TO);
-  nomp_test_chk(err);
-
-  err = nomp_update(a, 0, 10, sizeof(int), NOMP_FROM);
-  nomp_test_chk(err);
-  err = nomp_update(a, 0, 10, sizeof(int), NOMP_FROM);
-  nomp_test_chk(err);
-
-  return 0;
+  int err = 0;
+  TEST_BUILTIN_TYPES(050_multiple_d2h_calls, 0, 10);
+  TEST_BUILTIN_TYPES(050_multiple_d2h_calls, 5, 10);
+  TEST_BUILTIN_TYPES(050_multiple_d2h_calls, 2, 8);
+  return err;
 }
 
 // Check D2H followed by H2D.
 static int test_d2h_after_h2d() {
-  int a[10] = {0};
-
-  for (unsigned i = 0; i < 10; i++)
-    a[i] = i;
-  int err = nomp_update(a, 0, 10, sizeof(int), NOMP_TO);
-  nomp_test_chk(err);
-
-  for (unsigned i = 0; i < 10; i++)
-    a[i] = 0;
-
-  err = nomp_update(a, 0, 10, sizeof(int), NOMP_FROM);
-  nomp_test_chk(err);
-
-  for (unsigned i = 0; i < 10; i++)
-    nomp_test_assert(a[i] == i);
-
-  return 0;
+  int err = 0;
+  TEST_BUILTIN_TYPES(050_d2h_after_h2d, 0, 10);
+  TEST_BUILTIN_TYPES(050_d2h_after_h2d, 5, 10);
+  TEST_BUILTIN_TYPES(050_d2h_after_h2d, 2, 8);
+  return err;
 }
 
 // Free'ing after H2D is not an error.
 static int test_free_after_h2d() {
-  int a[10] = {0};
-  int err = nomp_update(a, 0, 10, sizeof(int), NOMP_TO);
-  nomp_test_chk(err);
-
-  err = nomp_update(a, 0, 10, sizeof(int), NOMP_FREE);
-  nomp_test_chk(err);
-
-  return 0;
+  int err = 0;
+  TEST_BUILTIN_TYPES(050_free_after_h2d, 0, 10);
+  TEST_BUILTIN_TYPES(050_free_after_h2d, 5, 10);
+  TEST_BUILTIN_TYPES(050_free_after_h2d, 2, 8);
+  return err;
 }
 
 // Free'ing after D2H is not an error.
 static int test_free_after_d2h() {
-  int a[10] = {0};
-  int err = nomp_update(a, 0, 10, sizeof(int), NOMP_TO);
-  nomp_test_chk(err);
-
-  err = nomp_update(a, 0, 10, sizeof(int), NOMP_FROM);
-  nomp_test_chk(err);
-
-  err = nomp_update(a, 0, 10, sizeof(int), NOMP_FREE);
-  nomp_test_chk(err);
-
-  return 0;
+  int err = 0;
+  TEST_BUILTIN_TYPES(050_free_after_d2h, 0, 10);
+  TEST_BUILTIN_TYPES(050_free_after_d2h, 5, 10);
+  TEST_BUILTIN_TYPES(050_free_after_d2h, 2, 8);
+  return err;
 }
 
 int main(int argc, const char *argv[]) {
