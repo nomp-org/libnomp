86461f191a22d9f3749360afd2e11e54622ab4b7
pubuduprabhathiya <pubuduprabhathiya@gmail.com>
2023-03-07 21:42:26 +0530

    Fix SYCL kernel wrapper function
    - Fix memory allocation issue
    - Fix 150, 210-240, test failures on SYCL backend

--- tests/nomp-api-150.c
+++ tests/nomp-api-150.c
@@ -22,7 +22,7 @@ static int test_invalid_kernel_id(int argc, const char **argv, int *id, int *a,
   nomp_test_assert(nomp_get_log_no(err) == NOMP_USER_INPUT_IS_INVALID);
 
   char *desc = nomp_get_log_str(err);
-  int eq = logcmp(desc, "\\[Error\\] .*\\/src\\/nomp.c:[0-9]* Kernel "
+  int eq = logcmp(desc, "\\[Error\\] .*\\/src\\/nomp.[c|cpp]:[0-9]* Kernel "
                         "id -1 passed to nomp_run is not valid.");
   nomp_free(desc);
   nomp_test_assert(eq);
