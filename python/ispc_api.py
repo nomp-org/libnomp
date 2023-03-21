b61575ba7562eeddc34d73f2cea244141aa26a3e
Hirumal Priyashan <hirupriyashanrc@gmail.com>
2023-03-21 18:01:56 +0530

    Add env variables for compilers

--- python/ispc_api.py
+++ python/ispc_api.py
@@ -4,42 +4,8 @@ import loopy as lp
 import numpy as np
 
 from loopy.target.c.compyte.dtypes import DTypeRegistry
+from loopy.target.ispc import fill_registry_with_ispc_types
 import cgen as c
-
-
-def fill_registry_with_ispc_types(reg):
-    """Add ispc types to the registry"""
-    reg.get_or_register_dtype("bool", bool)
-
-    reg.get_or_register_dtype(["int8", "signed char", "char"], np.int8)
-    reg.get_or_register_dtype(["uint8", "unsigned char"], np.uint8)
-    reg.get_or_register_dtype(
-        [
-            "int16",
-            "short",
-            "signed short",
-            "signed short int",
-            "short signed int",
-        ],
-        np.int16,
-    )
-    reg.get_or_register_dtype(
-        [
-            "uint16",
-            "unsigned short",
-            "unsigned short int",
-            "short unsigned int",
-        ],
-        np.uint16,
-    )
-    reg.get_or_register_dtype(["int32", "int", "signed int"], np.int32)
-    reg.get_or_register_dtype(["uint32", "unsigned", "unsigned int"], np.uint32)
-
-    reg.get_or_register_dtype(["int64"], np.int64)
-    reg.get_or_register_dtype(["uint64"], np.uint64)
-
-    reg.get_or_register_dtype("float", np.float32)
-    reg.get_or_register_dtype("double", np.float64)
 
 
 def get_arg(reg, index, value):
@@ -62,7 +28,7 @@ def get_ispc_entry_point(knl):
 def create_ispc_kernel_with_wrapper(knl):
     """Create ISPC kernel wrapper"""
     reg = DTypeRegistry()
-    fill_registry_with_ispc_types(reg)
+    fill_registry_with_ispc_types(reg, True)
     (entry,) = knl.entrypoints
     knl_args = [
         c.Value("", get_arg(reg, index, value))
@@ -73,9 +39,9 @@ def create_ispc_kernel_with_wrapper(knl):
     knl_name = gen_code.device_programs[0].name
     wrapper_name = get_ispc_entry_point(knl)
     param_count = len(knl_args)
-    calling_func = c.FunctionBody(
+    wrapper = c.FunctionBody(
         c.FunctionDeclaration(
-            c.Value("task void", wrapper_name),
+            c.Value("export void", wrapper_name),
             [
                 c.Value("void *uniform", "_p"),
             ],
@@ -100,16 +66,15 @@ def create_ispc_kernel_with_wrapper(knl):
             )
         ),
     )
-    entry_point = c.Line(
-        f"""#include "ispcrt.isph"\nDEFINE_CPU_ENTRY_POINT({wrapper_name})"""
-    )
-    content = list(map(str, [device_code, calling_func, entry_point]))
+    content = list(map(str, [device_code, wrapper]))
+
     return "\n\n".join(content).strip()
 
 
 if __name__ == "__main__":
     import os
     import sys
+    from loopy_api import c_to_loopy
 
     sys.path.append(os.path.dirname(os.path.abspath(__file__)))
     KNL_STR = """
@@ -125,5 +90,5 @@ if __name__ == "__main__":
             }
           }
           """
-    # lp_knl = c_to_loopy(KNL_STR, "ispc")
-    # print(lp.generate_code_v2(lp_knl).device_code())
+    lp_knl = c_to_loopy(KNL_STR, "ispc")
+    print(create_ispc_kernel_with_wrapper(lp_knl))
