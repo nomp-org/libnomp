3eebb938d803f3e92bbd709a1d9e0299b971f758
pubuduprabhathiya <pubuduprabhathiya@gmail.com>
2023-03-23 12:01:56 +0530

    Add compiler selection options
    - Add option to select SYCL implementation
    - Add option to select c compiler

--- python/kernel_wrapper.py
+++ python/kernel_wrapper.py
@@ -6,9 +6,8 @@ from loopy.target.c.compyte.dtypes import (
 )
 
 
-def get_arg_type(val):
-    arg_type = ""
-    dtype_reg = DTypeRegistry()
+def get_arg_type(val,dtype_reg):
+    arg_type = "" 
     fill_registry_with_c_types(dtype_reg, True)
     if type(val) == lp.kernel.data.ArrayArg:
         ctype = dtype_reg.dtype_to_ctype(val.dtype)
@@ -20,19 +19,16 @@ def get_arg_type(val):
         return arg_type
     return arg_type
 
-
-def set_args(args):
-    args_list = []
-    for i in range(len(args)):
-        args_list.append(c.Value(get_arg_type(args[i]), f"args[{i}]"))
-    return args_list
-
-
 def create_kernel_wrapper_fun(knl):
     """Create SYCL kernel wrapper"""
+    dtype_reg=DTypeRegistry()
     knl_name = lp.generate_code_v2(knl).device_programs[0].name
-    args_list = set_args(knl.callables_table[knl_name].subkernel.args)
-    args_length = len(knl.callables_table[knl_name].subkernel.args)
+    args = knl.callables_table[knl_name].subkernel.args
+    args_list = [
+        c.Value(get_arg_type(value,dtype_reg), f"args[{index}]")
+        for index, value in enumerate(args)
+    ]
+    args_length = len(args)
     grid_size = knl.default_entrypoint.get_grid_size_upper_bounds_as_exprs(
         knl.callables_table
     )
