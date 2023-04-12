10cbcf7f21c31ca0ed2b7a285f4a5347d8d0f13f
Thilina Ratnayaka <thilinarmtb@gmail.com>
2023-04-12 01:20:48 -0500

    Port bitwise and `continue` tests to new API

--- tests/nomp-api-242-impl.h
+++ tests/nomp-api-242-impl.h
@@ -1,16 +1,17 @@
 #include "nomp-test.h"
 
 #define nomp_api_242_aux TOKEN_PASTE(nomp_api_242_aux, TEST_SUFFIX)
-static int nomp_api_242_aux(const char *knl_fmt, const char **clauses,
-                            TEST_TYPE *a, int n) {
+static int nomp_api_242_aux(const char *fmt, TEST_TYPE *a, int n) {
   nomp_test_chk(nomp_update(a, 0, n, sizeof(TEST_TYPE), NOMP_TO));
 
   int id = -1;
-  nomp_test_chk(create_knl(&id, knl_fmt, clauses, 1, TOSTRING(TEST_TYPE),
-                           TOSTRING(TEST_TYPE)));
+  const char *clauses[4] = {"transform", "nomp-api-242", "transform", 0};
+  char *knl = generate_knl(fmt, 1, TOSTRING(TEST_TYPE));
+  nomp_test_chk(nomp_jit(&id, knl, clauses, 2, "a", sizeof(TEST_TYPE *),
+                         NOMP_PTR, "N", sizeof(int), NOMP_INT));
+  nomp_free(knl);
 
-  nomp_test_chk(nomp_run(id, 2, "a", NOMP_PTR, sizeof(TEST_TYPE), a, "N",
-                         NOMP_INT, sizeof(int), &n));
+  nomp_test_chk(nomp_run(id, a, &n));
 
   nomp_test_chk(nomp_sync());
 
@@ -29,13 +30,12 @@ static int nomp_api_242_bitwise_and(int n) {
   for (unsigned i = 0; i < n; i++)
     a[i] = i;
 
-  const char *knl_fmt =
+  const char *fmt =
       "void foo(%s *a, int N) {                                        \n"
       "  for (int i = 0; i < N; i++)                                   \n"
       "    a[i] = a[i] & 3;                                            \n"
       "}                                                               \n";
-  const char *clauses[4] = {"transform", "nomp-api-242", "transform", 0};
-  nomp_api_242_aux(knl_fmt, clauses, a, n);
+  nomp_api_242_aux(fmt, a, n);
 
   for (unsigned i = 0; i < n; i++)
     nomp_test_assert(a[i] == ((TEST_TYPE)i & 3));
@@ -53,13 +53,12 @@ static int nomp_api_242_bitwise_or(int n) {
   for (unsigned i = 0; i < n; i++)
     a[i] = i;
 
-  const char *knl_fmt =
+  const char *fmt =
       "void foo(%s *a, int N) {                                        \n"
       "  for (int i = 0; i < N; i++)                                   \n"
       "    a[i] = i | 3;                                               \n"
       "}                                                               \n";
-  const char *clauses[4] = {"transform", "nomp-api-242", "transform", 0};
-  nomp_api_242_aux(knl_fmt, clauses, a, n);
+  nomp_api_242_aux(fmt, a, n);
 
   for (unsigned i = 0; i < n; i++)
     nomp_test_assert(a[i] == ((TEST_TYPE)i | 3));
@@ -77,13 +76,12 @@ static int nomp_api_242_bitwise_xor(int n) {
   for (unsigned i = 0; i < n; i++)
     a[i] = i;
 
-  const char *knl_fmt =
+  const char *fmt =
       "void foo(%s *a, int N) {                                        \n"
       "  for (int i = 0; i < N; i++)                                   \n"
       "    a[i] = i ^ 3;                                               \n"
       "}                                                               \n";
-  const char *clauses[4] = {"transform", "nomp-api-242", "transform", 0};
-  nomp_api_242_aux(knl_fmt, clauses, a, n);
+  nomp_api_242_aux(fmt, a, n);
 
   for (unsigned i = 0; i < n; i++)
     nomp_test_assert(a[i] == ((TEST_TYPE)i ^ 3));
@@ -101,13 +99,12 @@ static int nomp_api_242_bitwise_left_shift(int n) {
   for (unsigned i = 0; i < n; i++)
     a[i] = i;
 
-  const char *knl_fmt =
+  const char *fmt =
       "void foo(%s *a, int N) {                                        \n"
       "  for (int i = 0; i < N; i++)                                   \n"
       "    a[i] = a[i] << 3;                                           \n"
       "}                                                               \n";
-  const char *clauses[4] = {"transform", "nomp-api-242", "transform", 0};
-  nomp_api_242_aux(knl_fmt, clauses, a, n);
+  nomp_api_242_aux(fmt, a, n);
 
   for (unsigned i = 0; i < n; i++)
     nomp_test_assert(a[i] == ((TEST_TYPE)i << 3));
@@ -125,13 +122,12 @@ static int nomp_api_242_bitwise_right_shift(int n) {
   for (unsigned i = 0; i < n; i++)
     a[i] = i;
 
-  const char *knl_fmt =
+  const char *fmt =
       "void foo(%s *a, int N) {                                        \n"
       "  for (int i = 0; i < N; i++)                                   \n"
       "    a[i] = a[i] >> 3;                                           \n"
       "}                                                               \n";
-  const char *clauses[4] = {"transform", "nomp-api-242", "transform", 0};
-  nomp_api_242_aux(knl_fmt, clauses, a, n);
+  nomp_api_242_aux(fmt, a, n);
 
   for (unsigned i = 0; i < n; i++)
     nomp_test_assert(a[i] == ((TEST_TYPE)i >> 3));
@@ -149,13 +145,12 @@ static int nomp_api_242_bitwise_complement(int n) {
   for (unsigned i = 0; i < n; i++)
     a[i] = i;
 
-  const char *knl_fmt =
+  const char *fmt =
       "void foo(%s *a, int N) {                                        \n"
       "  for (int i = 0; i < N; i++)                                   \n"
       "    a[i] = ~ a[i];                                              \n"
       "}                                                               \n";
-  const char *clauses[4] = {"transform", "nomp-api-242", "transform", 0};
-  nomp_api_242_aux(knl_fmt, clauses, a, n);
+  nomp_api_242_aux(fmt, a, n);
 
   for (unsigned i = 0; i < n; i++)
     nomp_test_assert(a[i] == (~(TEST_TYPE)i));
