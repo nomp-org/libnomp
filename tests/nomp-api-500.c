8c8baf80b337c88ab659b0f227832b11b0f2897f
Thilina Ratnayaka <thilinarmtb@gmail.com>
2023-04-29 01:00:52 -0500

    Add a sum reduction test

---
+++ tests/nomp-api-500.c
@@ -0,0 +1,20 @@
+#define TEST_IMPL_H "nomp-api-500-impl.h"
+#include "nomp-generate-tests.h"
+#undef TEST_IMPL_H
+
+static int test_sum_reduction() {
+  int err = 0;
+  TEST_BUILTIN_TYPES(500_sum, 10);
+  return err;
+}
+
+int main(int argc, const char *argv[]) {
+  nomp_check(nomp_init(argc, argv));
+
+  int err = 0;
+  err |= SUBTEST(test_sum_reduction);
+
+  nomp_check(nomp_finalize());
+
+  return err;
+}
