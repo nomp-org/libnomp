41f6b8bd17af4a4007fc6b34359043d69e137c8d
Thilina Ratnayaka <thilinarmtb@gmail.com>
2023-04-29 01:00:52 -0500

    Add a sum reduction test

---
+++ tests/nomp-api-500-impl.h
@@ -0,0 +1,50 @@
+#include "nomp-test.h"
+
+#define nomp_api_500_aux TOKEN_PASTE(nomp_api_500_aux, TEST_SUFFIX)
+static int nomp_api_500_aux(const char *fmt, const char **clauses, TEST_TYPE *a,
+                            int n, TEST_TYPE *sum) {
+  nomp_test_chk(nomp_update(a, 0, n, sizeof(TEST_TYPE), NOMP_TO));
+
+  int id = -1;
+  char *knl = generate_knl(fmt, 2, TOSTRING(TEST_TYPE), TOSTRING(TEST_TYPE));
+  nomp_test_chk(nomp_jit(&id, knl, clauses, 3, "a", sizeof(TEST_TYPE *),
+                         NOMP_PTR, "N", sizeof(int), NOMP_INT, "sum",
+                         sizeof(TEST_TYPE),
+                         TEST_NOMP_TYPE | NOMP_ATTRIBUTE_REDUCTION));
+  nomp_free(knl);
+
+  nomp_test_chk(nomp_run(id, a, &n, sum));
+
+  nomp_test_chk(nomp_sync());
+
+  nomp_test_chk(nomp_update(a, 0, n, sizeof(TEST_TYPE), NOMP_FREE));
+
+  return 0;
+}
+
+#define nomp_api_500_sum TOKEN_PASTE(nomp_api_500_sum, TEST_SUFFIX)
+static int nomp_api_500_sum(int N) {
+  nomp_test_assert(N <= 10);
+
+  TEST_TYPE a[10], sum;
+  for (unsigned i = 0; i < N; i++)
+    a[i] = i;
+
+  const char *knl_fmt =
+      "void foo(%s *a,  int N, %s *sum) {                              \n"
+      "  for (int i = 0; i < N; i++) {                                 \n"
+      "    sum[0] += a[i];                                             \n"
+      "  }                                                             \n"
+      "}                                                               \n";
+  const char *clauses[1] = {0};
+  nomp_api_500_aux(knl_fmt, clauses, a, N, &sum);
+
+#if defined(TEST_TOL)
+  nomp_test_assert(fabs(sum - (N - 1) * N / 2) < TEST_TOL);
+#else
+  nomp_test_assert(sum == (N - 1) * N / 2);
+#endif
+
+  return 0;
+}
+#undef nomp_api_500_sum
--- tests/nomp-generate-tests.h
+++ tests/nomp-generate-tests.h
@@ -1,43 +1,49 @@
+#define TEST_NOMP_TYPE NOMP_INT
 #define TEST_TYPE int
 #define TEST_SUFFIX _int
 #include TEST_IMPL_H
-#undef TEST_TYPE
 #undef TEST_SUFFIX
+#undef TEST_TYPE
 
 #define TEST_TYPE long
 #define TEST_SUFFIX _long
 #include TEST_IMPL_H
-#undef TEST_TYPE
 #undef TEST_SUFFIX
+#undef TEST_TYPE
+#undef TEST_NOMP_TYPE
 
+#define TEST_NOMP_TYPE NOMP_UINT
 #define TEST_TYPE unsigned
 #define TEST_SUFFIX _unsigned
 #include TEST_IMPL_H
-#undef TEST_TYPE
 #undef TEST_SUFFIX
+#undef TEST_TYPE
 
 #define TEST_TYPE unsigned long
 #define TEST_SUFFIX _unsigned_long
 #include TEST_IMPL_H
-#undef TEST_TYPE
 #undef TEST_SUFFIX
+#undef TEST_TYPE
+#undef TEST_NOMP_TYPE
 
 #ifndef TEST_INT_ONLY
 
+#define TEST_NOMP_TYPE NOMP_FLOAT
 #define TEST_TOL 1e-12
 #define TEST_TYPE double
 #define TEST_SUFFIX _double
 #include TEST_IMPL_H
-#undef TEST_TYPE
 #undef TEST_SUFFIX
+#undef TEST_TYPE
 #undef TEST_TOL
 
 #define TEST_TOL 1e-8
 #define TEST_TYPE float
 #define TEST_SUFFIX _float
 #include TEST_IMPL_H
-#undef TEST_TYPE
 #undef TEST_SUFFIX
+#undef TEST_TYPE
 #undef TEST_TOL
+#undef TEST_NOMP_TYPE
 
 #endif // TEST_INT_ONLY
