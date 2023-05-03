51364b38b74814e83720f16b15f82cc3c9172a91
nipunayf <nipunafe@gmail.com>
2023-05-03 22:33:56 +0530

    Add test for reduction with conditions

--- tests/nomp-api-500.c
+++ tests/nomp-api-500.c
@@ -8,11 +8,19 @@ static int test_sum_reduction() {
   return err;
 }
 
+static int test_sum_condition_reduction() {
+  int err = 0;
+  TEST_BUILTIN_TYPES(500_condition, 10);
+  return err;
+}
+
 int main(int argc, const char *argv[]) {
   nomp_check(nomp_init(argc, argv));
 
   int err = 0;
   err |= SUBTEST(test_sum_reduction);
+  /// TODO: Fix the errors regarding the kernel
+  //  err |= SUBTEST(test_sum_condition_reduction);
 
   nomp_check(nomp_finalize());
 
