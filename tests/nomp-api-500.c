6c256abce06387dca7c72bb4152db808a940b801
nipunayf <nipunafe@gmail.com>
2023-05-04 00:00:11 +0530

    Add test for reduction productions

--- tests/nomp-api-500.c
+++ tests/nomp-api-500.c
@@ -14,13 +14,27 @@ static int test_sum_condition_reduction() {
   return err;
 }
 
+static int test_mxm() {
+  int err = 0;
+  TEST_BUILTIN_TYPES(500_mxm, 10);
+  return err;
+}
+
+static int test_vxm() {
+  int err = 0;
+  TEST_BUILTIN_TYPES(500_vxm, 10);
+  return err;
+}
+
 int main(int argc, const char *argv[]) {
   nomp_check(nomp_init(argc, argv));
 
   int err = 0;
   err |= SUBTEST(test_sum_reduction);
-  /// TODO: Fix the errors regarding the kernel
+  /// TODO: Fix the errors of the following kernels
   //  err |= SUBTEST(test_sum_condition_reduction);
+  //  err |= SUBTEST(test_mxm);
+  //  err |= SUBTEST(test_vxm);
 
   nomp_check(nomp_finalize());
 
