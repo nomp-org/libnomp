4a80fa34463c8c7d2919b2c1920d00eb022546c8
nipunayf <nipunafe@gmail.com>
2023-05-03 22:33:56 +0530

    Add test for reduction with conditions

--- tests/nomp-api-500-impl.h
+++ tests/nomp-api-500-impl.h
@@ -48,3 +48,35 @@ static int nomp_api_500_sum(int N) {
   return 0;
 }
 #undef nomp_api_500_sum
+
+#define nomp_api_500_condition TOKEN_PASTE(nomp_api_500_condition, TEST_SUFFIX)
+static int nomp_api_500_condition(int N) {
+  nomp_test_assert(N <= 10);
+
+  TEST_TYPE a[N], sum;
+  const int mid_point = (int)N / 2;
+  for (int i = 0; i < mid_point; ++i)
+    a[i] = 0;
+  for (int i = mid_point; i < N; ++i)
+    a[i] = i;
+
+  const char *knl_fmt =
+      "void foo(%s *a, int N, %s *sum) {                               \n"
+      "  for (int i = 0; i < N; i++) {                                 \n"
+      "    if (a[i] > 0)                                               \n"
+      "      sum[0] += 1;                                              \n"
+      "  }                                                             \n"
+      "}                                                               \n";
+  const char *clauses[1] = {0};
+  nomp_api_500_aux(knl_fmt, clauses, a, N, &sum);
+
+#if defined(TEST_TOL)
+  nomp_test_assert(fabs(sum - mid_point) < TEST_TOL);
+#else
+  nomp_test_assert(sum == mid_point);
+#endif
+
+  return 0;
+}
+#undef nomp_api_500_condition
+#undef nomp_api_500_aux
