654f3d77d562f84f3000a648928ef3e2d7e7022f
NagithaAbeywickrema <nagitha98@gmail.com>
2023-05-11 12:07:14 +0530

    Add test for for loop bounds

---
+++ tests/nomp-api-243.c
@@ -0,0 +1,22 @@
+#define TEST_IMPL_H "nomp-api-243-impl.h"
+#include "nomp-generate-tests.h"
+#undef TEST_IMPL_H
+
+// Test non-constant/ non-single-variable for loop bounds inside a serial loop.
+static int test_for_loop_bounds() {
+  int err = 0;
+  TEST_BUILTIN_TYPES(243_for_loop_bounds, 10)
+  return err;
+}
+
+int main(int argc, const char *argv[]) {
+  int err = nomp_init(argc, argv);
+  nomp_check(err);
+
+  err |= SUBTEST(test_for_loop_bounds);
+
+  err |= nomp_finalize();
+  nomp_check(err);
+
+  return err;
+}
