77c31ea8f26926a49ee90b8f69f79ed48fa2302a
Thilina Ratnayaka <thilinarmtb@gmail.com>
2023-05-19 02:30:51 -0500

    Remove mem.c and move implementation to `nomp-mem.h`

--- src/nomp-mem.h
+++ src/nomp-mem.h
@@ -21,7 +21,11 @@ extern "C" {
  * @param line Line number in the file where the function is called.
  * @return int
  */
-int nomp_free_(void *p, const char *file, unsigned line);
+static int nomp_free_(void *p, const char *file, unsigned line) {
+  if (p)
+    free(p);
+  return 0;
+}
 
 /**
  * @ingroup nomp_mem_utils
@@ -35,17 +39,6 @@ int nomp_free_(void *p, const char *file, unsigned line);
 
 /**
  * @ingroup nomp_mem_utils
- * @brief Allocates memory blocks.
- *
- * @param size Bytes to allocate.
- * @param file Name of the file in which the function is called.
- * @param line Line number in the file where the function is called.
- * @return Void pointer
- */
-void *nomp_malloc_(size_t size, const char *file, unsigned line);
-
-/**
- * @ingroup nomp_mem_utils
  * @brief Helper macro for allocating memory blocks using nomp_malloc_().
  * File name and line number are passed implicitly.
  *
@@ -53,20 +46,7 @@ void *nomp_malloc_(size_t size, const char *file, unsigned line);
  * @param count Number of elements.
  * @return Pointer of type T.
  */
-#define nomp_malloc(T, count)                                                  \
-  ((T *)nomp_malloc_((count) * sizeof(T), __FILE__, __LINE__))
-
-/**
- * @ingroup nomp_mem_utils
- * @brief Allocates an array in memory with elements initialized to 0.
- *
- * @param nmemb Number of elements.
- * @param size Length in bytes of each element.
- * @param file Name of the file in which the function is called.
- * @param line Line number in the file where the function is called.
- * @return Void pointer
- */
-void *nomp_calloc_(size_t nmemb, size_t size, const char *file, unsigned line);
+#define nomp_malloc(T, count) ((T *)malloc((count) * sizeof(T)))
 
 /**
  * @ingroup nomp_mem_utils
@@ -78,20 +58,7 @@ void *nomp_calloc_(size_t nmemb, size_t size, const char *file, unsigned line);
  * @param count Number of elements.
  * @return Pointer of type T.
  */
-#define nomp_calloc(T, count)                                                  \
-  ((T *)nomp_calloc_((count), sizeof(T), __FILE__, __LINE__))
-
-/**
- * @ingroup nomp_mem_utils
- * @brief Reallocate memory blocks.
- *
- * @param ptr Pointer to the memory area to be reallocated.
- * @param size New size in bytes.
- * @param file Name of the file in which the function is called.
- * @param line Line number in the file where the function is called.
- * @return Void pointer
- */
-void *nomp_realloc_(void *ptr, size_t size, const char *file, unsigned line);
+#define nomp_calloc(T, count) ((T *)calloc((count), sizeof(T)))
 
 /**
  * @ingroup nomp_mem_utils
@@ -103,8 +70,7 @@ void *nomp_realloc_(void *ptr, size_t size, const char *file, unsigned line);
  * @param count Number of elements.
  * @return Pointer of type T
  */
-#define nomp_realloc(ptr, T, count)                                            \
-  ((T *)nomp_realloc_((ptr), (count) * sizeof(T), __FILE__, __LINE__))
+#define nomp_realloc(ptr, T, count) ((T *)realloc((ptr), (count) * sizeof(T)))
 
 #ifdef __cplusplus
 }
