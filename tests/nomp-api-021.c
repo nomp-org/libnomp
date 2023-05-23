a6e1a5ede2a840a6d62cb04a3574a6bd97cc4ba4
Thilina Rathnayake <thilinarmtb@gmail.com>
2023-05-23 06:18:54 -0400

    Remove tests which aren't valid in all backends
    Also, refactor a couple so that they will pass in all backends.

--- tests/nomp-api-021.c
+++ tests/nomp-api-021.c
@@ -1,49 +1,74 @@
 #include "nomp-test.h"
 #include <limits.h>
 
-// NOMP_nomp-backend environment variable with invalid value.
+#define set_test_env(VAR, ENVVAR, ENVVAL)                                      \
+  {                                                                            \
+    VAR = nomp_copy_env(ENVVAR, NOMP_TEST_MAX_BUFSIZ);                         \
+    setenv(ENVVAR, ENVVAL, 1);                                                 \
+  }
+
+#define reset_env(VAR, ENVVAR)                                                 \
+  {                                                                            \
+    if (VAR)                                                                   \
+      setenv(ENVVAR, VAR, 1);                                                  \
+    else                                                                       \
+      unsetenv(ENVVAR);                                                        \
+    nomp_free(&VAR);                                                           \
+  }
+
+// NOMP_BACKEND environment variable with invalid value.
 static int test_invalid_nomp_backend(int argc, const char **argv) {
-  setenv("NOMP_BACKEND", "invalid", 1);
-  nomp_test_assert(nomp_get_log_no(nomp_init(argc, argv)) ==
-                   NOMP_USER_INPUT_IS_INVALID);
-  unsetenv("NOMP_BACKEND");
+  char *backend = NULL;
+  set_test_env(backend, "NOMP_BACKEND", "invalid");
+
+  int err = nomp_init(argc, argv);
+  nomp_test_assert(nomp_get_log_no(err) == NOMP_USER_INPUT_IS_INVALID);
 
   nomp_test_assert(nomp_get_log_no(nomp_finalize()) == NOMP_FINALIZE_FAILURE);
+
+  reset_env(backend, "NOMP_BACKEND");
 
   return 0;
 }
 
 // NOMP_PLATFORM environment variable with invalid value.
 static int test_invalid_platform_id(int argc, const char **argv) {
-  setenv("NOMP_PLATFORM", "invalid", 1);
-  nomp_test_assert(nomp_get_log_no(nomp_init(argc, argv)) ==
-                   NOMP_USER_INPUT_IS_INVALID);
-  unsetenv("NOMP_PLATFORM");
+  char *platform = NULL;
+  set_test_env(platform, "NOMP_PLATFORM", "invalid");
+
+  int err = nomp_init(argc, argv);
+  nomp_test_assert(nomp_get_log_no(err) == NOMP_USER_INPUT_IS_INVALID);
 
   nomp_test_assert(nomp_get_log_no(nomp_finalize()) == NOMP_FINALIZE_FAILURE);
+
+  reset_env(platform, "NOMP_PLATFORM");
 
   return 0;
 }
 
 // NOMP_DEVICE environment variable with invalid value.
 static int test_invalid_device_id(int argc, const char **argv) {
-  setenv("NOMP_DEVICE", "invalid", 1);
-  nomp_test_assert(nomp_get_log_no(nomp_init(argc, argv)) ==
-                   NOMP_USER_INPUT_IS_INVALID);
-  unsetenv("NOMP_DEVICE");
+  char *device = NULL;
+  set_test_env(device, "NOMP_DEVICE", "invalid");
+
+  int err = nomp_init(argc, argv);
+  nomp_test_assert(nomp_get_log_no(err) == NOMP_USER_INPUT_IS_INVALID);
 
   nomp_test_assert(nomp_get_log_no(nomp_finalize()) == NOMP_FINALIZE_FAILURE);
+
+  reset_env(device, "NOMP_DEVICE");
 
   return 0;
 }
 
 // NOMP_VERBOSE environment variable with invalid value.
 static int test_invalid_nomp_verbose(int argc, const char **argv) {
-  setenv("NOMP_VERBOSE", "4", 1);
-  int err = nomp_init(argc, argv);
-  unsetenv("NOMP_VERBOSE");
+  char *verbose = NULL;
+  set_test_env(verbose, "NOMP_VERBOSE", "4");
 
+  int err = nomp_init(argc, argv);
   nomp_test_assert(nomp_get_log_no(err) == NOMP_USER_INPUT_IS_INVALID);
+
   char *desc = nomp_get_log_str(err);
   int eq = logcmp(
       desc, "\\[Error\\] .*libnomp\\/src\\/log.c:[0-9]* Invalid verbose level "
@@ -53,79 +78,66 @@ static int test_invalid_nomp_verbose(int argc, const char **argv) {
 
   nomp_test_assert(nomp_get_log_no(nomp_finalize()) == NOMP_FINALIZE_FAILURE);
 
-  return 0;
-}
-
-// Run with a valid NOMP_nomp-backend environment variable.
-static int test_valid_nomp_backend(int argc, const char **argv) {
-  setenv("NOMP_BACKEND", "opencl", 1);
-  nomp_test_chk(nomp_init(argc, argv));
-  unsetenv("NOMP_BACKEND");
-
-  nomp_test_chk(nomp_finalize());
-
-  return 0;
-}
-
-// NOMP_nomp-backend value is not case sensitive.
-static int test_nomp_backend_case_insensitivity(int argc, const char **argv) {
-  setenv("NOMP_BACKEND", "oPenCl", 1);
-  nomp_test_chk(nomp_init(argc, argv));
-  unsetenv("NOMP_BACKEND");
-
-  nomp_test_chk(nomp_finalize());
+  reset_env(verbose, "NOMP_VERBOSE");
 
   return 0;
 }
 
 // Run with a valid NOMP_PLATFORM environment variable.
 static int test_valid_platform_id(int argc, const char **argv) {
-  setenv("NOMP_PLATFORM", "0", 1);
-  nomp_test_chk(nomp_init(argc, argv));
-  unsetenv("NOMP_PLATFORM");
+  char *platform = NULL;
+  set_test_env(platform, "NOMP_PLATFORM", "0");
 
+  nomp_test_chk(nomp_init(argc, argv));
   nomp_test_chk(nomp_finalize());
+
+  reset_env(platform, "NOMP_PLATFORM");
 
   return 0;
 }
 
 // Run with a valid NOMP_DEVICE  environment variable.
 static int test_valid_device_id(int argc, const char **argv) {
-  setenv("NOMP_DEVICE", "0", 1);
-  nomp_test_chk(nomp_init(argc, argv));
-  unsetenv("NOMP_DEVICE");
+  char *device = NULL;
+  set_test_env(device, "NOMP_DEVICE", "0");
 
+  nomp_test_chk(nomp_init(argc, argv));
   nomp_test_chk(nomp_finalize());
+
+  reset_env(device, "NOMP_DEVICE");
 
   return 0;
 }
 
 int main(int argc, const char *argv[]) {
-  int err = 0, argsc = 6;
-  const char *args0[6] = {"--nomp-backend",  "opencl", "--nomp-device", "0",
-                          "--nomp-platform", "0"};
-  err |= SUBTEST(test_invalid_nomp_backend, argsc, args0);
-  err |= SUBTEST(test_invalid_platform_id, argsc, args0);
-  err |= SUBTEST(test_invalid_device_id, argsc, args0);
-  err |= SUBTEST(test_invalid_nomp_verbose, argsc, args0);
+  int err = 0;
+  err |= SUBTEST(test_invalid_nomp_backend, argc, argv);
+  err |= SUBTEST(test_invalid_platform_id, argc, argv);
+  err |= SUBTEST(test_invalid_device_id, argc, argv);
+  err |= SUBTEST(test_invalid_nomp_verbose, argc, argv);
 
-  const char *args1[6] = {"--nomp-backend",  "invalid", "--nomp-device", "0",
-                          "--nomp-platform", "0"};
-  err |= SUBTEST(test_valid_nomp_backend, argsc, args1);
-  err |= SUBTEST(test_nomp_backend_case_insensitivity, argsc, args1);
+  nomp_test_assert(argc <= 64);
+  char *argvn[64];
 
-  char *max_int = nomp_calloc(char, 100);
-  snprintf(max_int, 100, "%d", INT_MAX);
+  // Copy everything but `--nomp-platform` to new command line args.
+  int argcn = 0;
+  for (unsigned i = 0; i < argc; i++) {
+    if (strncmp(argv[i], "--nomp-platform", NOMP_TEST_MAX_BUFSIZ))
+      argvn[argcn] = strndup(argv[i], NOMP_TEST_MAX_BUFSIZ), argcn++;
+  }
+  err |= SUBTEST(test_valid_platform_id, argcn, (const char **)argvn);
+  for (unsigned i = 0; i < argcn; i++)
+    nomp_free(&argvn[i]);
 
-  const char *args2[6] = {"--nomp-backend",  "opencl", "--nomp-device", "0",
-                          "--nomp-platform", max_int};
-  err |= SUBTEST(test_valid_platform_id, argsc, args2);
-
-  const char *args3[6] = {"--nomp-backend", "opencl",          "--nomp-device",
-                          max_int,          "--nomp-platform", "0"};
-  err |= SUBTEST(test_valid_device_id, argsc, args3);
-
-  nomp_free(&max_int);
+  // Copy everything but `--nomp-device` to new command line args.
+  argcn = 0;
+  for (unsigned i = 0; i < argc; i++) {
+    if (strncmp(argv[i], "--nomp-device", NOMP_TEST_MAX_BUFSIZ))
+      argvn[argcn] = strndup(argv[i], NOMP_TEST_MAX_BUFSIZ), argcn++;
+  }
+  err |= SUBTEST(test_valid_device_id, argcn, (const char **)argvn);
+  for (unsigned i = 0; i < argcn; i++)
+    nomp_free(&argvn[i]);
 
   return err;
 }
