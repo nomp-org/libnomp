e920bbe600fb6f95b699a5a04b0b934b75f16467
Thilina Rathnayake <thilinarmtb@gmail.com>
2023-05-26 18:27:12 -0400

    Move test 220 to 205 and use a poroper transform
    and minor fixes in 102.py and 200.py

--- tests/nomp-api-200.py
+++ tests/nomp-api-200.py
@@ -9,6 +9,5 @@ def transform(knl, context):
     knl = lp.split_iname(
         knl, iname, 32, inner_iname=i_inner, outer_iname=i_outer
     )
-    knl = lp.tag_inames(knl, {i_outer: "g.0"})
-    knl = lp.tag_inames(knl, {i_inner: "l.0"})
+    knl = lp.tag_inames(knl, {i_outer: "g.0", i_inner: "l.0"})
     return knl
