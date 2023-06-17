f68dfe1c77fa06c402a61d0fd3236b7577df7990
Thilina Rathnayake <thilinarmtb@gmail.com>
2023-06-17 05:10:03 -0400

    Refactor HIP and Cuda backends

--- src/unified-cuda-hip-impl.h
+++ src/unified-cuda-hip-impl.h
@@ -3,30 +3,18 @@
 #define TOKEN_PASTE_(a, b) a##b
 #define TOKEN_PASTE(a, b) TOKEN_PASTE_(a, b)
 
-#define gpu_update TOKEN_PASTE(GPU, _update)
-#define gpu_update_ptr TOKEN_PASTE(GPU, _update_ptr)
-#define gpu_knl_build TOKEN_PASTE(GPU, _knl_build)
-#define gpu_knl_run TOKEN_PASTE(GPU, _knl_run)
-#define gpu_knl_free TOKEN_PASTE(GPU, _knl_free)
-#define gpu_sync TOKEN_PASTE(GPU, _sync)
-#define gpu_finalize TOKEN_PASTE(GPU, _finalize)
-#define gpu_init TOKEN_PASTE(GPU, _init)
-#define gpu_backend TOKEN_PASTE(GPU, _backend)
-#define gpu_prog TOKEN_PASTE(GPU, _prog)
-#define gpu_compile TOKEN_PASTE(GPU, _compile)
-
-#define gpuError_t TOKEN_PASTE(GPU, Error_t)
-#define gpuSuccess TOKEN_PASTE(GPU, Success)
-#define gpuGetErrorName TOKEN_PASTE(GPU, GetErrorName)
-#define gpuMalloc TOKEN_PASTE(GPU, Malloc)
-#define gpuMemcpy TOKEN_PASTE(GPU, Memcpy)
-#define gpuFree TOKEN_PASTE(GPU, Free)
-#define gpuMemcpyHostToDevice TOKEN_PASTE(GPU, MemcpyHostToDevice)
-#define gpuMemcpyDeviceToHost TOKEN_PASTE(GPU, MemcpyDeviceToHost)
-#define gpuDeviceSynchronize TOKEN_PASTE(GPU, DeviceSynchronize)
-#define gpuGetDeviceCount TOKEN_PASTE(GPU, GetDeviceCount)
-#define gpuSetDevice TOKEN_PASTE(GPU, SetDevice)
-#define gpuGetDeviceProperties TOKEN_PASTE(GPU, GetDeviceProperties)
+#define gpuError_t TOKEN_PASTE(BACKEND, Error_t)
+#define gpuSuccess TOKEN_PASTE(BACKEND, Success)
+#define gpuGetErrorName TOKEN_PASTE(BACKEND, GetErrorName)
+#define gpuMalloc TOKEN_PASTE(BACKEND, Malloc)
+#define gpuMemcpy TOKEN_PASTE(BACKEND, Memcpy)
+#define gpuFree TOKEN_PASTE(BACKEND, Free)
+#define gpuMemcpyHostToDevice TOKEN_PASTE(BACKEND, MemcpyHostToDevice)
+#define gpuMemcpyDeviceToHost TOKEN_PASTE(BACKEND, MemcpyDeviceToHost)
+#define gpuDeviceSynchronize TOKEN_PASTE(BACKEND, DeviceSynchronize)
+#define gpuGetDeviceCount TOKEN_PASTE(BACKEND, GetDeviceCount)
+#define gpuSetDevice TOKEN_PASTE(BACKEND, SetDevice)
+#define gpuGetDeviceProperties TOKEN_PASTE(BACKEND, GetDeviceProperties)
 
 #define gpurtcResult TOKEN_PASTE(RUNTIME, Result)
 #define gpurtcGetErrorString TOKEN_PASTE(RUNTIME, GetErrorString)
@@ -37,38 +25,44 @@
 #define gpurtcGetProgramLog TOKEN_PASTE(RUNTIME, GetProgramLog)
 #define gpurtcDestroyProgram TOKEN_PASTE(RUNTIME, DestroyProgram)
 
-#define chk_err_(file, line, call, err_t, success_t, retrive_err, type)        \
-  do {                                                                         \
-    err_t result = (call);                                                     \
-    if (result != success_t) {                                                 \
-      const char *msg;                                                         \
-      retrive_err;                                                             \
-      return nomp_log(NOMP_GPU_FAILURE, NOMP_ERROR, ERR_STR_GPU_FAILURE, type, \
-                      msg);                                                    \
+#define chk_err(CALL, ERR_T, SUCCES, GETERR, OP)                               \
+  {                                                                            \
+    ERR_T result = (CALL);                                                     \
+    if (result != SUCCES) {                                                    \
+      const char *msg = GETERR(result);                                        \
+      return nomp_log(NOMP_BACKEND_FAILURE, NOMP_ERROR,                        \
+                      ERR_STR_BACKEND_FAILURE, OP, msg);                       \
     }                                                                          \
-  } while (0)
+  }
 
 #define chk_gpu(call)                                                          \
-  chk_err_(__FILE__, __LINE__, call, gpuError_t, gpuSuccess,                   \
-           msg = gpuGetErrorName(result), "operation");
+  chk_err(call, gpuError_t, gpuSuccess, gpuGetErrorName, "operation");
 
-#define chk_gpurtc(call)                                                       \
-  chk_err_(__FILE__, __LINE__, call, gpurtcResult, GPURTC_SUCCESS,             \
-           msg = gpurtcGetErrorString(result), "runtime compilation")
+#define chk_gpu_rt(call)                                                       \
+  chk_err(call, gpurtcResult, GPURTC_SUCCESS, gpurtcGetErrorString, "runtime")
 
-struct gpu_backend {
+#define gpu_backend_t TOKEN_PASTE(BACKEND, _backend_t)
+struct gpu_backend_t {
   int device_id;
-  struct gpuDeviceProp prop;
-  gpuCtx ctx;
+  gpuDeviceProp_t prop;
 };
 
-struct gpu_prog {
+#define gpu_prog_t TOKEN_PASTE(BACKEND, _prog_t)
+struct gpu_prog_t {
   gpuModule module;
   gpuFunction kernel;
 };
 
-gpurtcResult gpu_compile(gpurtcProgram prog, struct gpu_backend *nbnd);
+#define gpu_compile TOKEN_PASTE(BACKEND, _compile)
+static gpurtcResult gpu_compile(gpurtcProgram prog, struct gpu_backend_t *bnd) {
+  char arch[NOMP_MAX_BUFSIZ];
+  snprintf(arch, NOMP_MAX_BUFSIZ, "-arch=compute_%d%d", bnd->prop.major,
+           bnd->prop.minor);
+  const char *opts[2] = {arch, NULL};
+  return gpurtcCompileProgram(prog, 1, opts);
+}
 
+#define gpu_update TOKEN_PASTE(BACKEND, _update)
 static int gpu_update(struct nomp_backend_t *bnd, struct nomp_mem_t *m,
                       const nomp_map_direction_t op, size_t start, size_t end,
                       size_t usize) {
@@ -95,53 +89,56 @@ static int gpu_update(struct nomp_backend_t *bnd, struct nomp_mem_t *m,
   return 0;
 }
 
+#define gpu_update_ptr TOKEN_PASTE(BACKEND, _update_ptr)
 static void gpu_update_ptr(void **p, size_t *size, struct nomp_mem_t *m) {
-  *p = (void *)m->bptr;
-  *size = sizeof(m->bptr);
+  *p = (void *)m->bptr, *size = sizeof(m->bptr);
 }
 
+#define gpu_knl_build TOKEN_PASTE(BACKEND, _knl_build)
 static int gpu_knl_build(struct nomp_backend_t *bnd, struct nomp_prog_t *prg,
                          const char *source, const char *name) {
-  struct gpu_backend *nbnd = (struct gpu_backend *)bnd->bptr;
+  struct gpu_backend_t *gbnd = (struct gpu_backend_t *)bnd->bptr;
 
   gpurtcProgram prog;
-  chk_gpurtc(gpurtcCreateProgram(&prog, source, NULL, 0, NULL, NULL));
+  chk_gpu_rt(gpurtcCreateProgram(&prog, source, NULL, 0, NULL, NULL));
 
-  gpurtcResult result = gpu_compile(prog, nbnd);
+  gpurtcResult result = gpu_compile(prog, gbnd);
   if (result != GPURTC_SUCCESS) {
+    const char *err = gpurtcGetErrorString(result);
+
     size_t size;
     gpurtcGetProgramLogSize(prog, &size);
     char *log = nomp_calloc(char, size + 1);
     gpurtcGetProgramLog(prog, log);
-    const char *err_str = gpurtcGetErrorString(result);
-    size += strlen(err_str) + 2 + 1;
 
+    size += strlen(err) + 2 + 1;
     char *msg = nomp_calloc(char, size);
-    snprintf(msg, size, "%s: %s", err_str, log);
-    int err_id = nomp_log(NOMP_GPU_FAILURE, NOMP_ERROR, ERR_STR_GPU_FAILURE,
-                          "build", msg);
-    nomp_free(&log), nomp_free(&msg);
-    return err_id;
+    snprintf(msg, size, "%s: %s", err, log);
+    int ret = nomp_log(NOMP_BACKEND_FAILURE, NOMP_ERROR,
+                       ERR_STR_BACKEND_FAILURE, "build", msg);
+    nomp_free(&msg), nomp_free(&log);
+    return ret;
   }
 
-  size_t code_size;
-  chk_gpurtc(gpurtcGetCodeSize(prog, &code_size));
-  char *code = nomp_calloc(char, code_size);
-  chk_gpurtc(gpurtcGetCode(prog, code));
+  size_t size;
+  chk_gpu_rt(gpurtcGetCodeSize(prog, &size));
+  char *code = nomp_calloc(char, size + 1);
+  chk_gpu_rt(gpurtcGetCode(prog, code));
+  chk_gpu_rt(gpurtcDestroyProgram(&prog));
 
-  struct gpu_prog *nprg = prg->bptr = nomp_calloc(struct gpu_prog, 1);
-  GPU_CHECK(gpuModuleLoadData(&nprg->module, code));
-  GPU_CHECK(gpuModuleGetFunction(&nprg->kernel, nprg->module, name));
-
+  struct gpu_prog_t *gprg = nomp_calloc(struct gpu_prog_t, 1);
+  check(gpuModuleLoadData(&gprg->module, code));
   nomp_free(&code);
-  chk_gpurtc(gpurtcDestroyProgram(&prog));
+  check(gpuModuleGetFunction(&gprg->kernel, gprg->module, name));
+  prg->bptr = (void *)gprg;
 
   return 0;
 }
 
+#define gpu_knl_run TOKEN_PASTE(BACKEND, _knl_run)
 static int gpu_knl_run(struct nomp_backend_t *bnd, struct nomp_prog_t *prg) {
-  void *vargs[NOMP_MAX_KNL_ARGS];
   struct nomp_arg_t *args = prg->args;
+  void *vargs[NOMP_MAX_KNL_ARGS];
   for (int i = 0; i < prg->nargs; i++) {
     if (args[i].type == NOMP_PTR)
       vargs[i] = &args[i].ptr;
@@ -150,58 +147,53 @@ static int gpu_knl_run(struct nomp_backend_t *bnd, struct nomp_prog_t *prg) {
   }
 
   const size_t *global = prg->global, *local = prg->local;
-  struct gpu_prog *cprg = (struct gpu_prog *)prg->bptr;
-  GPU_CHECK(gpuModuleLaunchKernel(cprg->kernel, global[0], global[1], global[2],
-                                  local[0], local[1], local[2], 0, NULL, vargs,
-                                  NULL));
+  struct gpu_prog_t *gprg = (struct gpu_prog_t *)prg->bptr;
+  check(gpuModuleLaunchKernel(gprg->kernel, global[0], global[1], global[2],
+                              local[0], local[1], local[2], 0, NULL, vargs,
+                              NULL));
+
   return 0;
 }
 
+#define gpu_knl_free TOKEN_PASTE(BACKEND, _knl_free)
 static int gpu_knl_free(struct nomp_prog_t *prg) {
-  struct gpu_prog *cprg = (struct gpu_prog *)prg->bptr;
-
-  if (cprg)
-    GPU_CHECK(gpuModuleUnload(cprg->module));
+  struct gpu_prog_t *gprg = (struct gpu_prog_t *)prg->bptr;
+  if (gprg)
+    check(gpuModuleUnload(gprg->module));
 
   return 0;
 }
 
+#define gpu_sync TOKEN_PASTE(BACKEND, _sync)
 static int gpu_sync(struct nomp_backend_t *bnd) {
   chk_gpu(gpuDeviceSynchronize());
   return 0;
 }
 
+#define gpu_finalize TOKEN_PASTE(BACKEND, _finalize)
 static int gpu_finalize(struct nomp_backend_t *bnd) {
-#ifndef __HIP_PLATFORM_HCC__
-  struct gpu_backend *nbnd = (struct gpu_backend *)bnd->bptr;
-  if (nbnd)
-    GPU_CHECK(gpuCtxDestroy(nbnd->ctx));
-#endif
   nomp_free(&bnd->bptr);
   return 0;
 }
 
+#define gpu_init TOKEN_PASTE(BACKEND, _init)
 int gpu_init(struct nomp_backend_t *bnd, const int platform_id,
              const int device_id) {
   int num_devices;
-  GPU_CHECK(gpuGetDeviceCount(&num_devices));
+  check(gpuGetDeviceCount(&num_devices));
   if (device_id < 0 || device_id >= num_devices) {
     return nomp_log(NOMP_USER_INPUT_IS_INVALID, NOMP_ERROR,
                     ERR_STR_USER_DEVICE_IS_INVALID, device_id);
   }
 
   chk_gpu(gpuSetDevice(device_id));
+  check(gpuFree(0));
 
-  struct gpu_backend *nbnd = nomp_calloc(struct gpu_backend, 1);
-  nbnd->device_id = device_id;
-  chk_gpu(gpuGetDeviceProperties(&nbnd->prop, device_id));
+  struct gpu_backend_t *gbnd = nomp_calloc(struct gpu_backend_t, 1);
+  gbnd->device_id = device_id;
+  chk_gpu(gpuGetDeviceProperties(&gbnd->prop, device_id));
 
-#ifndef __HIP_PLATFORM_HCC__
-  GPU_CHECK(gpuInit(0));
-  GPU_CHECK(gpuCtxCreate(&nbnd->ctx, 0, nbnd->device_id));
-#endif
-  bnd->bptr = (void *)nbnd;
-
+  bnd->bptr = (void *)gbnd;
   bnd->update = gpu_update;
   bnd->knl_build = gpu_knl_build;
   bnd->knl_run = gpu_knl_run;
@@ -212,23 +204,22 @@ int gpu_init(struct nomp_backend_t *bnd, const int platform_id,
   return 0;
 }
 
-#undef TOKEN_PASTE_
-#undef TOKEN_PASTE
-#undef chk_err_
-#undef chk_gpu
-#undef chk_gpurtc
-
-#undef gpu_update
-#undef gpu_update_ptr
-#undef gpu_knl_build
-#undef gpu_knl_run
-#undef gpu_knl_free
-#undef gpu_sync
-#undef gpu_finalize
 #undef gpu_init
-#undef gpu_backend
-#undef gpu_prog
+#undef gpu_finalize
+#undef gpu_sync
+#undef gpu_knl_free
+#undef gpu_knl_run
+#undef gpu_knl_build
+#undef gpu_update_ptr
+#undef gpu_update
 #undef gpu_compile
+
+#undef gpu_prog_t
+#undef gpu_backend_t
+
+#undef chk_gpu_rt
+#undef chk_gpu
+#undef chk_err
 
 #undef gpuError_t
 #undef gpuSuccess
@@ -251,3 +242,6 @@ int gpu_init(struct nomp_backend_t *bnd, const int platform_id,
 #undef gpurtcGetProgramLogSize
 #undef gpurtcGetProgramLog
 #undef gpurtcDestroyProgram
+
+#undef TOKEN_PASTE
+#undef TOKEN_PASTE_
