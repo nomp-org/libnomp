f756be1eddccaec74c3c77d0ef59c1694d18b6d0
Thilina Ratnayaka <thilinarmtb@gmail.com>
2023-09-20 21:59:49 -0500

    Update docs for auxiliary functions

--- include/nomp-aux.h
+++ include/nomp-aux.h
@@ -14,15 +14,18 @@ extern "C" {
 
 /**
  * @defgroup nomp_other_utils Other helper functions.
+ *
+ * @brief Various helper functions used for implementing the library.
  */
 
 /**
  * @ingroup nomp_other_utils
+ *
  * @brief Concatenates \p n strings.
  *
  * Concatenates \p n strings and returns a pointer to the resulting string.
- * Each string should be at most \p max_len length long. Returned pointer
- * need to be freed using nomp_free().
+ * Each string should be at most \p max_len length long. User has to free memory
+ * allocated for the resulting string using nomp_free().
  *
  * @param[in] n Number of strings to concatenate.
  * @param[in] max_len Maximum length of an individual string.
@@ -33,10 +36,11 @@ char *nomp_str_cat(unsigned n, unsigned max_len, ...);
 
 /**
  * @ingroup nomp_other_utils
- * @brief Convert a string to unsigned long value if possible.
  *
- * Convert input string \p str to an unsigned int value. Returns converted
- * unsigned int value if successful, otherwise return -1. \p size denotes
+ * @brief Try to convert input string \p str to an non-negative integer value.
+ *
+ * Convert input string \p str to an non-negative int value. Returns converted
+ * non-negative int value if successful, otherwise return -1. \p size denotes
  * the maximum length of the string \p str.
  *
  * @param[in] str String to convert into unsigned int.
@@ -47,10 +51,43 @@ int nomp_str_toui(const char *str, size_t size);
 
 /**
  * @ingroup nomp_other_utils
+ *
+ * @brief Returns the maximum among all integers passed.
+ *
+ * Returns the maximum between \p n integers.
+ *
+ * @param[in] n Total number of integers.
+ * @param[in] ... List of integers.
+ * @return int
+ */
+int nomp_max(unsigned n, ...);
+
+/**
+ * @ingroup nomp_other_utils
+ *
+ * @brief Get a copy of the environment variable value if it exists.
+ *
+ * Get a copy of the value of the environment variable \p name as a string if
+ * it is defined. Return NULL if the environment variable is not set. \p size
+ * is the maximum length of the string. User must free the memory allocated for
+ * the resulting string using nomp_free().
+ *
+ * @param[in] name Name of the environment variable.
+ * @param[in] size Maximum length of the environment variable value.
+ * @return char *
+ */
+char *nomp_copy_env(const char *name, size_t size);
+
+/**
+ * @ingroup nomp_other_utils
+ *
  * @brief Returns the length of a posix complaint path.
  *
- * If \p len is not NULL, it is set to the length of the path if the path length
- * resoultion was successful. Otherwise, it is set to zero.
+ * If \p len is not NULL, it is set to the length of the \p path if the \p path
+ * length resolution was successful. Otherwise, it is set to zero. On success,
+ * the function returns zero. Otherwise, it returns an error id which can be
+ * used to query the error id and string using nomp_get_err_str() and
+ * nomp_get_err_id().
  *
  * @param[out] len Lenth of path specified in \p path.
  * @param[in] path Path to get the maximum length.
@@ -60,39 +97,18 @@ int nomp_path_len(size_t *len, const char *path);
 
 /**
  * @ingroup nomp_other_utils
- * @brief Returns maximum among all integers passed.
  *
- * Returns the maximum between two or more integers.
- *
- * @param[in] n Total number of integers.
- * @param[in] ... List of integers to find the maximum of as a variable argument
- * list.
- * @return int
- */
-int nomp_max(unsigned n, ...);
-
-/**
- * @ingroup nomp_other_utils
  * @brief Check if the python script path exists.
  *
- * \p path shouldn't contain the ".py" extension.
+ * Check if there is a python script at the path \p path. Returns 0 if the
+ * path exists, otherwise returns an error id which can be used to query the
+ * error id and string using nomp_get_err_str() and nomp_get_err_id(). The \p
+ * path should be provided without the ".py" extension.
  *
- * @param[in] path Total number of integers.
+ * @param[in] path Path to the python script without the ".py" extension.
  * @return int
  */
 int nomp_check_py_script_path(const char *path);
-
-/**
- * @ingroup nomp_other_utils
- * @brief Get a copy of the environment variable \p name if exists.
- *
- * Return NULL if the environment variable is not set.
- *
- * @param[in] name Name of the environment variable.
- * @param[in] size Maximum lenght of the environment variable value.
- * @return char *
- */
-char *nomp_copy_env(const char *name, size_t size);
 
 #ifdef __cplusplus
 }
