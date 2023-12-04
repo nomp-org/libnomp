1d0b0532799831786759f441938bc3d14e369abc
Thilina Rathnayake <thilinarmtb@gmail.com>
2023-12-04 15:29:17 -0500

    Get rid of test 105, refactor test 100 and 150

--- tests/nomp-api-100.c
+++ tests/nomp-api-100.c
@@ -12,9 +12,27 @@ static const char *invalid_knl =
     "    a[i] = i                                                         \n"
     "}                                                                    \n";
 
-// Calling nomp_jit() with missing file should return an error.
-static int test_missing_transform_file(void) {
-  const char *clauses[4] = {"transform", "missing-file", "missing", 0};
+#define TEST_IMPL_H "nomp-api-100-impl.h"
+#include "nomp-generate-tests.h"
+#undef TEST_IMPL_H
+
+// Calling nomp_jit() with invalid file should return an error.
+static int test_invalid_transform_file(void) {
+  int err = 0;
+  TEST_BUILTIN_TYPES(100_invalid_file, );
+  return err;
+}
+
+// Calling nomp_jit() with invalid function should return an error.
+static int test_invalid_transform_function(void) {
+  int err = 0;
+  TEST_BUILTIN_TYPES(100_invalid_function, );
+  return err;
+}
+
+// Calling nomp_jit() with empty file name should return an error.
+static int test_empty_filename(void) {
+  const char *clauses[4] = {"transform", NULL, "tile", 0};
 
   static int id = -1;
   int err = nomp_jit(&id, valid_knl, clauses, 2, "a", sizeof(int), NOMP_PTR,
@@ -22,17 +40,17 @@ static int test_missing_transform_file(void) {
   nomp_test_assert(nomp_get_err_no(err) == NOMP_USER_INPUT_IS_INVALID);
 
   char *log = nomp_get_err_str(err);
-  int eq = logcmp(log, "\\[Error\\] .*src\\/.*.c:[0-9]* Python module "
-                       "\"missing-file\" not found.");
+  int eq = logcmp(log, "\\[Error\\] .*src\\/.*.c:[0-9]* Module "
+                       "name and/or function name not provided.");
   nomp_free(&log);
   nomp_test_assert(eq);
 
   return 0;
 }
 
-// Calling nomp_jit() with missing function should return an error.
-static int test_missing_transform_function(void) {
-  const char *clauses[4] = {"transform", "nomp_api_100", "missing_func", 0};
+// Calling nomp_jit() with empty transform function name should return an error.
+static int test_empty_user_callback(void) {
+  const char *clauses[4] = {"transform", "nomp_api_100", NULL, 0};
 
   static int id = -1;
   int err = nomp_jit(&id, valid_knl, clauses, 2, "a", sizeof(int), NOMP_PTR,
@@ -40,9 +58,8 @@ static int test_missing_transform_function(void) {
   nomp_test_assert(nomp_get_err_no(err) == NOMP_USER_INPUT_IS_INVALID);
 
   char *log = nomp_get_err_str(err);
-  int eq =
-      logcmp(log, "\\[Error\\] .*src\\/.*.c:[0-9]* Python function "
-                  "\"missing_func\" not found in module \"nomp_api_100\".");
+  int eq = logcmp(log, "\\[Error\\] .*src\\/.*.c:[0-9]* Module "
+                       "name and/or function name not provided.");
   nomp_free(&log);
   nomp_test_assert(eq);
 
@@ -68,43 +85,7 @@ static int test_invalid_clause(void) {
   return 0;
 }
 
-// Empty file name in nomp_jit() should return an error.
-static int test_empty_filename(void) {
-  const char *clauses[4] = {"transform", NULL, "tile", 0};
-
-  static int id = -1;
-  int err = nomp_jit(&id, valid_knl, clauses, 2, "a", sizeof(int), NOMP_PTR,
-                     "N", sizeof(int), NOMP_INT);
-  nomp_test_assert(nomp_get_err_no(err) == NOMP_USER_INPUT_IS_INVALID);
-
-  char *log = nomp_get_err_str(err);
-  int eq = logcmp(log, "\\[Error\\] .*src\\/.*.c:[0-9]* Module "
-                       "name and/or function name not provided.");
-  nomp_free(&log);
-  nomp_test_assert(eq);
-
-  return 0;
-}
-
-// Empty user callback in nomp_jit() should return an error.
-static int test_empty_user_callback(void) {
-  const char *clauses[4] = {"transform", "nomp_api_100", NULL, 0};
-
-  static int id = -1;
-  int err = nomp_jit(&id, valid_knl, clauses, 2, "a", sizeof(int), NOMP_PTR,
-                     "N", sizeof(int), NOMP_INT);
-  nomp_test_assert(nomp_get_err_no(err) == NOMP_USER_INPUT_IS_INVALID);
-
-  char *log = nomp_get_err_str(err);
-  int eq = logcmp(log, "\\[Error\\] .*src\\/.*.c:[0-9]* Module "
-                       "name and/or function name not provided.");
-  nomp_free(&log);
-  nomp_test_assert(eq);
-
-  return 0;
-}
-
-// The kernel has a syntax error due to a missing a semicolon.
+// Calling nomp_jit() with a kernel which has a syntax error should fail.
 static int test_syntax_error_in_kernel(void) {
   static int id = -1;
   const char *clauses0[4] = {"transform", "nomp_api_100", "tile", 0};
@@ -145,19 +126,18 @@ static int test_syntax_error_in_transform_function(void) {
 }
 
 int main(int argc, const char *argv[]) {
-  int err = nomp_init(argc, argv);
-  nomp_test_check(err);
+  nomp_test_check(nomp_init(argc, argv));
 
-  err |= SUBTEST(test_missing_transform_file);
-  err |= SUBTEST(test_missing_transform_function);
+  int err = 0;
+  err |= SUBTEST(test_invalid_transform_file);
+  err |= SUBTEST(test_invalid_transform_function);
   err |= SUBTEST(test_invalid_clause);
   err |= SUBTEST(test_empty_filename);
   err |= SUBTEST(test_empty_user_callback);
   err |= SUBTEST(test_syntax_error_in_kernel);
   err |= SUBTEST(test_syntax_error_in_transform_function);
 
-  err = nomp_finalize();
-  nomp_test_check(err);
+  nomp_test_check(nomp_finalize());
 
   return err;
 }
---
+++ tests/nomp-api-150-impl.h
@@ -0,0 +1,73 @@
+#include "nomp-test.h"
+
+// Calling nomp_run() with invalid kernel_id should return an error.
+#define nomp_api_150_invalid_kernel_id                                         \
+  TOKEN_PASTE(nomp_api_150_invalid_kernel_id, TEST_SUFFIX)
+static int nomp_api_150_invalid_kernel_id(int n) {
+  assert(n <= TEST_MAX_SIZE);
+
+  const char *fmt = "void foo(%s *a, %s *b, int N) {                        \n"
+                    "  for (int i = 0; i < N; i++)                          \n"
+                    "    a[i] = a[i] * b[i];                                \n"
+                    "}                                                      \n";
+
+  char *knl = generate_knl(fmt, 2, TOSTRING(TEST_TYPE), TOSTRING(TEST_TYPE));
+
+  int id = -1;
+  const char *clauses[4] = {"transform", "nomp_api_100", "tile", 0};
+  nomp_test_check(nomp_jit(&id, knl, clauses, 3, "a", sizeof(TEST_TYPE),
+                           NOMP_PTR, "b", sizeof(TEST_TYPE), NOMP_PTR, "N",
+                           sizeof(int), NOMP_INT));
+  nomp_free(&knl);
+
+  TEST_TYPE a[TEST_MAX_SIZE], b[TEST_MAX_SIZE];
+  int err = nomp_run(-1, a, b, &n);
+  nomp_test_assert(nomp_get_err_no(err) == NOMP_USER_INPUT_IS_INVALID);
+
+  char *log = nomp_get_err_str(err);
+  int eq = logcmp(log, "\\[Error\\] .*\\/src\\/nomp.[c|cpp]:[0-9]* Kernel "
+                       "id -1 passed to nomp_run is not valid.");
+  nomp_free(&log);
+  nomp_test_assert(eq);
+
+  return 0;
+}
+#undef nomp_api_150_invalid_kernel_id
+
+// Calling nomp_run() should fail because `b` is not mapped.
+#define nomp_api_150_unmapped_array                                            \
+  TOKEN_PASTE(nomp_api_150_unmapped_array, TEST_SUFFIX)
+static int nomp_api_150_unmapped_array(int n) {
+  assert(n <= TEST_MAX_SIZE);
+
+  const char *fmt = "void foo(%s *a, %s *b, int N) {                        \n"
+                    "  for (int i = 0; i < N; i++)                          \n"
+                    "    a[i] = a[i] * b[i];                                \n"
+                    "}                                                      \n";
+
+  char *knl = generate_knl(fmt, 2, TOSTRING(TEST_TYPE), TOSTRING(TEST_TYPE));
+
+  int id = -1;
+  const char *clauses[4] = {"transform", "nomp_api_100", "tile", 0};
+  nomp_test_check(nomp_jit(&id, knl, clauses, 3, "a", sizeof(TEST_TYPE),
+                           NOMP_PTR, "b", sizeof(TEST_TYPE), NOMP_PTR, "N",
+                           sizeof(int), NOMP_INT));
+  nomp_free(&knl);
+
+  TEST_TYPE a[TEST_MAX_SIZE], b[TEST_MAX_SIZE];
+  nomp_test_check(nomp_update(a, 0, n, sizeof(TEST_TYPE), NOMP_TO));
+
+  int err = nomp_run(id, a, b, &n);
+  nomp_test_assert(nomp_get_err_no(err) == NOMP_USER_MAP_PTR_IS_INVALID);
+
+  char *desc = nomp_get_err_str(err);
+  int eq = logcmp(desc, "\\[Error\\] .*\\/src\\/.*.[c|cpp]:[0-9]* Map pointer "
+                        "0[xX][0-9a-fA-F]* was not found on device.");
+  nomp_free(&desc);
+  nomp_test_assert(eq);
+
+  nomp_test_check(nomp_update(a, 0, n, sizeof(TEST_TYPE), NOMP_FREE));
+
+  return 0;
+}
+#undef nomp_api_150_unmapped_array
