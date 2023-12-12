4a5b23ab52251a0851146b9d1ab28ade36941785
Thilina Rathnayake <thilinarmtb@gmail.com>
2023-12-12 12:01:51 -0500

    Add 3D fixed and variable length array tests

--- tests/nomp-api-400-impl.h
+++ tests/nomp-api-400-impl.h
@@ -172,7 +172,6 @@ static int nomp_api_400_fixed_size_2d_array(int n) {
   return 0;
 }
 #undef nomp_api_400_fixed_size_2d_array
-#undef nomp_api_400_static_aux
 
 #define nomp_api_400_dynamic_2d_array                                          \
   TOKEN_PASTE(nomp_api_400_dynamic_2d_array, TEST_SUFFIX)
@@ -212,4 +211,81 @@ static int nomp_api_400_dynamic_2d_array(int n, int m) {
   return 0;
 }
 #undef nomp_api_400_dynamic_2d_array
+
+#define nomp_api_400_fixed_size_3d_array                                       \
+  TOKEN_PASTE(nomp_api_400_fixed_size_3d_array, TEST_SUFFIX)
+static int nomp_api_400_fixed_size_3d_array(int n) {
+  nomp_test_assert(n <= TEST_MAX_SIZE / 32);
+
+  const char *knl_fmt =
+      "void foo(%s *b, const %s *a, int n) {                           \n"
+      "  for (int i = 0; i < n; i++) {                                 \n"
+      "    %s s[2][2][32];                                             \n"
+      "    for (int j = 0; j < 32; j++)                                \n"
+      "      s[1][1][j] = a[i * 32 + j];                               \n"
+      "    for (int j = 0; j < 32; j++)                                \n"
+      "      s[1][1][j] += s[1][1][j];                                 \n"
+      "    for (int j = 0; j < 32; j++)                                \n"
+      "      b[i * 32 + j] = s[1][1][j];                               \n"
+      "  }                                                             \n"
+      "}                                                               \n";
+
+  TEST_TYPE a[TEST_MAX_SIZE], b[TEST_MAX_SIZE];
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < 32; j++)
+      a[i * 32 + j] = i;
+  }
+
+  nomp_api_400_static_aux(knl_fmt, b, a, n);
+
+#if defined(TEST_TOL)
+  for (int i = 0; i < 32 * n; i++)
+    nomp_test_assert(fabs(b[i] - (TEST_TYPE)(2 * a[i])) < TEST_TOL);
+#else
+  for (int i = 0; i < 32 * n; i++)
+    nomp_test_assert(b[i] == (TEST_TYPE)(2 * a[i]));
+#endif
+
+  return 0;
+}
+#undef nomp_api_400_fixed_size_3d_array
+#undef nomp_api_400_static_aux
+
+#define nomp_api_400_dynamic_3d_array                                          \
+  TOKEN_PASTE(nomp_api_400_dynamic_3d_array, TEST_SUFFIX)
+static int nomp_api_400_dynamic_3d_array(int n, int m) {
+  nomp_test_assert(n * m <= TEST_MAX_SIZE);
+
+  const char *knl_fmt =
+      "void foo(%s *b, const %s *a, int n, int m) {                    \n"
+      "  for (int i = 0; i < n; i++) {                                 \n"
+      "    %s s[m][m][m];                                              \n"
+      "    for (int j = 0; j < m; j++)                                 \n"
+      "      s[j][0][0] = a[i * m + j];                                \n"
+      "    for (int j = 0; j < m; j++)                                 \n"
+      "      s[j][0][0] += s[j][0][0];                                 \n"
+      "    for (int j = 0; j < m; j++)                                 \n"
+      "      b[i * m + j] = s[j][0][0];                                \n"
+      "  }                                                             \n"
+      "}                                                               \n";
+
+  TEST_TYPE a[TEST_MAX_SIZE], b[TEST_MAX_SIZE];
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++)
+      a[i * m + j] = i;
+  }
+
+  nomp_api_400_dynamic_aux(knl_fmt, b, a, n, m);
+
+#if defined(TEST_TOL)
+  for (int i = 0; i < n * m; i++)
+    nomp_test_assert(fabs(b[i] - (TEST_TYPE)(2 * a[i])) < TEST_TOL);
+#else
+  for (int i = 0; i < n * m; i++)
+    nomp_test_assert(b[i] == (TEST_TYPE)(2 * a[i]));
+#endif
+
+  return 0;
+}
+#undef nomp_api_400_dynamic_3d_array
 #undef nomp_api_400_dynamic_aux
--- tests/nomp-api-400.c
+++ tests/nomp-api-400.c
@@ -30,6 +30,19 @@ static int test_variable_length_2d_array(void) {
   return err;
 }
 
+static int test_fixed_size_3d_array(void) {
+  int err = 0;
+  TEST_BUILTIN_TYPES(400_fixed_size_3d_array, 16);
+  return err;
+}
+
+static int test_variable_length_3d_array(void) {
+  int err = 0;
+  TEST_BUILTIN_TYPES(400_dynamic_3d_array, 16, 8);
+  TEST_BUILTIN_TYPES(400_dynamic_3d_array, 16, 4);
+  return err;
+}
+
 int main(int argc, const char **argv) {
   nomp_test_check(nomp_init(argc, argv));
 
@@ -38,6 +51,8 @@ int main(int argc, const char **argv) {
   err |= SUBTEST(test_variable_length_1d_array);
   err |= SUBTEST(test_fixed_size_2d_array);
   err |= SUBTEST(test_variable_length_2d_array);
+  err |= SUBTEST(test_fixed_size_3d_array);
+  err |= SUBTEST(test_variable_length_3d_array);
 
   nomp_test_check(nomp_finalize());
 
